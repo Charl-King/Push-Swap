3594ddb0395ab32ef20b1e45249b0908ec56f1c4
charl KING <cking@c1r2s4.wethinkcode.co.za>
2018-08-02 15:46:24 +0200

    321

--- test.c
+++ test.c
@@ -1,14 +1,22 @@
+
 #include "list.h"
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    t_list l1;
-    t_list l2;
-    l1.data = 1;
-    l1.next = &l2;
-    l2.data = 2;
-    l2.next = NULL;
-    printf("%d\n",l1.next->data);
+	t_list l1;
+	t_list l2;
+	t_list *tmp;
+
+	l1.data = 5;
+	l1.next = &l2;
+	l1.next->data = 5;
+	tmp = l1.next;
+
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	printf("%d\n", l1.next->data);
 }
