7fabad38737d15952c4d960d3e03fb05ce295009
charl KING <cking@c1r2s4.wethinkcode.co.za>
2018-08-02 16:25:07 +0200

    still testing

--- test.c
+++ test.c
@@ -5,17 +5,28 @@
 
 int		main(int argc, char **argv)
 {
-	t_list l1;
-	t_list l2;
-	t_list *tmp;
+	t_list	l1;
+	t_list	l2;
+	t_list	*tmp;
+	int		i;
 
 	l1.data = 5;
 	l1.next = &l2;
 	l1.next->data = 5;
-	tmp = l1.next;
+	tmp = &l1;
+	i = 0;
 
-	while (tmp->next)
+	while (tmp->next && i < 10)
 	{
+		tmp = tmp->next;
+		tmp->data = i;
+	}
+
+	l1 = *tmp;
+
+	while (l1.next)
+	{
+		printf("%d\n", l1.data);
 		tmp = tmp->next;
 	}
 	printf("%d\n", l1.next->data);
